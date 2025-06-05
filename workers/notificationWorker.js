f42df55c1fbe1ef60d78ee5f728d781bc1cec1a6
pkb32 <113055313+pkb32@users.noreply.github.com>
2025-06-05 18:44:09 +0530

    added a dummy http server to avoid killing

--- workers/notificationWorker.js
+++ workers/notificationWorker.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const { Worker } = require('bullmq');
 const IORedis = require('ioredis');
 const mongoose = require('mongoose');
@@ -7,6 +8,20 @@ require('dotenv').config();
 const connection = require('../queues/redisConnection'); 
 
 mongoose.connect(process.env.MONGODB_URI);
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Notification Worker is up and running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Worker HTTP server listening on port ${PORT}`);
+});
+
 
 const worker = new Worker('notifications', async job => {
   console.log('Received job:', job.id, job.data);  // log the received job
